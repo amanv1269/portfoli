cc2c07dc31b58e726664fe35d5132d46a7d20765
amanv1269 <168532444+amanv1269@users.noreply.github.com>
2024-08-13 23:55:20 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profile from "../assets/kevinRushprofile.jpg"
+import profile from "../assets/profile.png"
 import { motion } from "framer-motion";
 const container = (delay) => ({
   hidden: { x: -100, opacity: 0 },
