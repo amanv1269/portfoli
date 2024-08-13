079da3c7b6f7f6d9c78604a3405ad1ac9eb00d74
amanv1269 <168532444+amanv1269@users.noreply.github.com>
2024-08-13 23:47:23 +0530

    add

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/kevinRushprofile.png";
+import profile from "../assets/kevinRushprofile.jpg"
 import { motion } from "framer-motion";
 const container = (delay) => ({
   hidden: { x: -100, opacity: 0 },
@@ -48,7 +48,7 @@ const Hero = () => {
               initial={{ x: 100, opacity: 0 }}
               animate={{ x: 0, opacity: 1 }}
               transition={{ duration: 1, delay: 1.2 }}
-              src={profilePic}
+              src={profile}
               alt="profilePic"
             />
           </div>
